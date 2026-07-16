7eee947aa819fd694b692c74340897f25097e749
kyle-elly <kyle.elly.2026@outlook.com>
2026-07-16 13:49:35 -0500

    Update prune_gallery.py

--- scripts/prune_gallery.py
+++ scripts/prune_gallery.py
@@ -16,7 +16,6 @@ from googleapiclient.discovery import build
 FOLDER_ID = os.environ["GDRIVE_FOLDER_ID"]
 SA_FILE   = os.environ["GDRIVE_SA_FILE"]
 THUMB_DIR = Path("thumbnails")
-LARGE_DIR = Path("large")
 MANIFEST  = Path("manifest.json")
 SCOPES    = ["https://www.googleapis.com/auth/drive.readonly"]
 
@@ -81,7 +80,6 @@ def main() -> int:
     # Actually prune
     for fid in stale:
         (THUMB_DIR / f"{fid}.jpg").unlink(missing_ok=True)
-        (LARGE_DIR / f"{fid}.jpg").unlink(missing_ok=True)
         del manifest[fid]
 
     ordered = sorted(manifest.values(),
